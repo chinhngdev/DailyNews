7b62f1418e99065338e82dedc60ffb665087f933
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-06-04 15:20:35 +0700

    Initialize project

---
+++ DailyNews/DailyNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNews
+//
+//  Created by Chinh on 6/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
