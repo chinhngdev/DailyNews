13d8487759f9ad132128667b1cb23bd7eba5820f
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-06-14 09:45:19 +0700

    Refactor project to remove storyboard and set up UI programmatically

--- DailyNews/DailyNews/SceneDelegate.swift
+++ DailyNews/DailyNews/SceneDelegate.swift
@@ -13,10 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // Đảm bảo scene là UIWindowScene
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Tạo window mới với windowScene
+        window = UIWindow(windowScene: windowScene)
+        
+        // Tạo instance của ViewController
+        let viewController = ViewController()
+        
+        // Đặt ViewController làm root view controller
+        window?.rootViewController = viewController
+        
+        // Hiển thị window
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
