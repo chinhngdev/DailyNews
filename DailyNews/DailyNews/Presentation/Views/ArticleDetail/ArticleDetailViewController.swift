d2211ffd4ce616daf1c5eb26bd972c5d35478f20
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-07-24 09:24:25 +0700

    Create ArticleDetailViewController.swift

---
+++ DailyNews/DailyNews/Presentation/Views/ArticleDetail/ArticleDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleDetailViewController.swift
+//  DailyNews
+//
+//  Created by Chinh on 7/23/25.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
