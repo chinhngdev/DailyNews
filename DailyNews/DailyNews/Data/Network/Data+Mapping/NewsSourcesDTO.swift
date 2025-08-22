9759ccd985691ca72a2dd980d7b5d85660fdc54e
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-08-22 11:32:28 +0700

    Rename ArticleResponseDTO+Mapping and add NewsSourcesDTO

---
+++ DailyNews/DailyNews/Data/Network/Data+Mapping/NewsSourcesDTO.swift
@@ -0,0 +1,23 @@
+//
+//  NewsSourcesDTO.swift
+//  DailyNews
+//
+//  Created by Chinh on 8/22/25.
+//
+
+import Foundation
+
+struct NewsSourcesResponseDTO: Decodable {
+    let status: String
+    let sources: [NewsSourceDTO]
+}
+
+struct NewsSourceDTO: Decodable {
+    let id: String
+    let name: String
+    let description: String
+    let url: String
+    let category: String
+    let language: String
+    let country: String
+}
