4508719c283dc2f8d6842506cfde61436454c22b
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-08-25 17:37:51 +0700

    Refactor NewsRepository to use NetworkService for API requests
    - Updated NewsRepository to utilize NetworkService for handling network requests, improving code modularity and testability.

--- DailyNews/DailyNews/Data/Repositories/NewsRepository.swift
+++ DailyNews/DailyNews/Data/Repositories/NewsRepository.swift
@@ -8,70 +8,16 @@
 import Foundation
 
 final class NewsRepository {
+    private let networkService: NetworkServiceProtocol
     
-    private let urlSession: URLSession
-    
-    /// The repository to query the news from the database
-    // private let newsQueriesRepository: NewsQueriesRepository
-    
-    // MARK: - Initializer
-    init(urlSession: URLSession = .shared) {
-        self.urlSession = urlSession
+    init(networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
     }
-    
 }
 
 extension NewsRepository: NewsRepositoryProtocol {
     func getNews(with requestValue: FetchNewsRequest) async throws -> ArticleResponseDTO {
-        var urlComponents = URLComponents(string: APIConfiguration.newsAPIBaseURL)
-        urlComponents?.path = "/v2/everything"
-        urlComponents?.queryItems = [
-            URLQueryItem(name: "q", value: requestValue.query),
-            URLQueryItem(name: "sortBy", value: requestValue.sortBy.rawValue),
-            URLQueryItem(name: "pageSize", value: String(requestValue.pageSize)),
-            URLQueryItem(name: "page", value: String(requestValue.page))
-        ]
-
-        // Khi urlCoponents?.url được gọi thì Swift sẽ tự động xây dựng URL hoàn chỉnh bao gồm cả query parameters từ queryItems
-        // Ví dụ: https://newsapi.org/v2/everything?q=technology&sortBy=relevancy&pageSize=20&page=1
-        guard let url = urlComponents?.url else {
-            throw NewsError.invalidURL
-        }
-        
-        var request = URLRequest(url: url)
-        request.setValue(APIConfiguration.newsAPIKey, forHTTPHeaderField: "Authorization")
-        
-        // Set timeout for avoiding hang forever
-        request.timeoutInterval = APIConfiguration.apiTimeout
-        
-        do {
-            let (data, response) = try await urlSession.data(for: request)
-            
-            // Check for HTTP status code
-            guard let httpResponse = response as? HTTPURLResponse else {
-                throw NewsError.invalidResponse
-            }
-            
-            switch httpResponse.statusCode {
-            case 200...299:
-                break
-            case 401:
-                throw NewsError.unauthorized
-            case 429:
-                throw NewsError.tooManyRequests
-            case 500:
-                throw NewsError.serverError
-            default:
-                throw NewsError.invalidResponse
-            }
-            
-            // Decode JSON
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .useDefaultKeys
-            let news = try decoder.decode(ArticleResponseDTO.self, from: data)
-            return news
-        } catch let error as NewsError {
-            throw error
-        }
+        let router = NewsRouter.everything(requestValue)
+        return try await networkService.request(router, responseType: ArticleResponseDTO.self)
     }
 }
